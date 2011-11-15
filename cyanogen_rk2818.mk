6d8ec1f4b1956bed3f9ac6b6835aea78d4d1eb9a
FUKAUMI Naoki <naobsd@gmail.com>
2011-11-15 13:53:44 +0900

    use common_versions.mk
    7.2.0-RC0 now

--- cyanogen_rk2818.mk
+++ cyanogen_rk2818.mk
@@ -18,26 +18,15 @@ PRODUCT_BRAND := rockchip
 PRODUCT_DEVICE := rk2818
 PRODUCT_MODEL := RK2818
 PRODUCT_MANUFACTURER := Rockchip
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=rk2818 BUILD_ID=GRK39F BUILD_DISPLAY_ID=GWK74 BUILD_FINGERPRINT=google/soju/crespo:2.3.6/GRK39F/189904:user/release-keys PRIVATE_BUILD_DESC="soju-user 2.3.6 GRK39F 189904 release-keys" BUILD_NUMBER=189904
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=rk2818 BUILD_ID=GRK39F BUILD_FINGERPRINT=google/soju/crespo:2.3.6/GRK39F/189904:user/release-keys PRIVATE_BUILD_DESC="soju-user 2.3.6 GRK39F 189904 release-keys" BUILD_NUMBER=189904
 
 # Extra RK2818 overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/rockchip/rk2818/overlay
 
-#
-# Set ro.modversion
-#
-ifdef CYANOGEN_NIGHTLY
-    PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-RK2818
-else
-    ifdef CYANOGEN_RELEASE
-        PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-RK2818
-    else
-        PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-RK2818-KANG
-    endif
-endif
+# Release name and versioning
+PRODUCT_RELEASE_NAME := RK2818
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+-include vendor/cyanogen/products/common_versions.mk
 
 #
 # Copy RK2818 specific prebuilt files
